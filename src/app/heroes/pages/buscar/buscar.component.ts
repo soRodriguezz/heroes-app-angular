0915dc5b483bae6b4474dea98164b5d1e92d130c
soRodriguezz <s.rodriguezzapata01@gmail.com>
2021-07-06 23:16:55 -0400

    end autocomplete

--- src/app/heroes/pages/buscar/buscar.component.ts
+++ src/app/heroes/pages/buscar/buscar.component.ts
@@ -13,7 +13,7 @@ export class BuscarComponent implements OnInit {
 
   termino: string = '';
   heroes: Heroe[] = [];
-  heroeSeleccionado!: Heroe;
+  heroeSeleccionado!: Heroe | undefined;
 
   constructor( private heroesService: HeroesService ) { }
 
@@ -21,15 +21,21 @@ export class BuscarComponent implements OnInit {
   }
 
   buscando(){
-    this.heroesService.getSugerencias( this.termino )
+    this.heroesService.getSugerencias( this.termino.trim() )
       .subscribe( heroes => this.heroes = heroes )
   }
 
   opcionSeleccionada( event: MatAutocompleteSelectedEvent ){
+
+    if(!event.option.value){
+      this.heroeSeleccionado = undefined;
+      return;
+    }
+
     const heroe: Heroe = event.option.value;
     this.termino = heroe.superhero;
 
-    this.heroesService.getHeroePorId( heroe.id )
+    this.heroesService.getHeroePorId( heroe.id! )
       .subscribe( heroe => this.heroeSeleccionado = heroe );
   }
 
